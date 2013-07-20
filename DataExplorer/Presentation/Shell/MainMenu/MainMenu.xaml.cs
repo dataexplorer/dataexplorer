0aa2c4911d8c7fa7168d6b6c5e457030da1186db
Matthew Renze <matthew@renzeconsulting.com>
2013-07-20 14:45:52 -0500

    Created basic window layout Added ShowHeader property to AvalonDock DLLs

---
+++ DataExplorer/Presentation/Shell/MainMenu/MainMenu.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataExplorer.Presentation.Shell.MainMenu
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : UserControl
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
