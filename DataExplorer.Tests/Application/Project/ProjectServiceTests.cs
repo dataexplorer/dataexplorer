f6adbe89e48e21aa5a829a9d5acc33cd6d3c65e4
Matthew Renze <matthew@renzeconsulting.com>
2013-08-12 21:17:52 -0500

    Added missing files

--- DataExplorer.Tests/Application/Project/ProjectServiceTests.cs
+++ DataExplorer.Tests/Application/Project/ProjectServiceTests.cs
@@ -5,11 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using DataExplorer.Application.Project;
 using DataExplorer.Application.Serialization;
+using DataExplorer.Domain.Columns;
 using DataExplorer.Domain.Events;
 using DataExplorer.Domain.Projects;
 using DataExplorer.Domain.Rows;
 using DataExplorer.Domain.ScatterPlots;
 using DataExplorer.Domain.Views;
+using DataExplorer.Persistence.Columns;
 using Moq;
 using NUnit.Framework;
 
@@ -20,36 +22,44 @@ namespace DataExplorer.Tests.Application.Project
     {
         private ProjectService _service;
         private Mock<ISerializationService> _mockSerializationService;
-        private Mock<IViewRepository> _mockViewRepository;
+        private Mock<IColumnRepository> _mockColumnRepository;
         private Mock<IRowRepository> _mockRowRepository;
-        private List<IScatterPlot> _views;
-        private IScatterPlot _view;
-        private List<Row> _rows;
+        private Mock<IViewRepository> _mockViewRepository;
+        private Column _column;
+        private List<Column> _columns;
         private Row _row;
+        private List<Row> _rows;
+        private IScatterPlot _view;
+        private List<IScatterPlot> _views;
 
         [SetUp]
         public void SetUp()
         {
-            _view = new DataExplorer.Domain.ScatterPlots.ScatterPlot();
-            _views = new List<IScatterPlot> { _view };
+            _column = new Column(1, "Column 1");
+            _columns = new List<Column> { _column };
             _row = new Row();
             _rows = new List<Row> { _row };
-            _mockViewRepository = new Mock<IViewRepository>();
+            _view = new DataExplorer.Domain.ScatterPlots.ScatterPlot();
+            _views = new List<IScatterPlot> { _view };
+            _mockColumnRepository = new Mock<IColumnRepository>();
             _mockRowRepository = new Mock<IRowRepository>();
+            _mockViewRepository = new Mock<IViewRepository>();
             _mockSerializationService = new Mock<ISerializationService>();
-            _mockSerializationService.Setup(p => p.GetViews()).Returns(_views);
+            _mockSerializationService.Setup(p => p.GetColumns()).Returns(_columns);
             _mockSerializationService.Setup(p => p.GetRows()).Returns(_rows);
+            _mockSerializationService.Setup(p => p.GetViews()).Returns(_views);
             _service = new ProjectService(
                 _mockSerializationService.Object,
-                _mockViewRepository.Object,
-                _mockRowRepository.Object);
+                _mockColumnRepository.Object,
+                _mockRowRepository.Object,
+                _mockViewRepository.Object);
         }
 
         [Test]
-        public void TestOpenProjectShouldAddViewsToTheViewRepository()
+        public void TestOpenProjectShouldAddColumnsToTheRepository()
         {
             _service.OpenProject();
-            _mockViewRepository.Verify(p => p.Add(_view));
+            _mockColumnRepository.Verify(p => p.Add(_column), Times.Once());
         }
 
         [Test]
@@ -57,6 +67,13 @@ namespace DataExplorer.Tests.Application.Project
         {
             _service.OpenProject();
             _mockRowRepository.Verify(p => p.Add(_row), Times.Once());
+        }
+
+        [Test]
+        public void TestOpenProjectShouldAddViewsToTheViewRepository()
+        {
+            _service.OpenProject();
+            _mockViewRepository.Verify(p => p.Add(_view));
         }
 
         [Test]
---
+++ DataExplorer.Tests/Persistence/Columns/ColumnRepositoryTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataExplorer.Domain.Columns;
+using DataExplorer.Persistence.Columns;
+using Moq;
+using NUnit.Framework;
+
+namespace DataExplorer.Tests.Persistence.Columns
+{
+    [TestFixture]
+    public class ColumnRepositoryTests
+    {
+        private ColumnRepository _repository;
+        private Mock<IColumnContext> _mockContext;
+        private List<Column> _columns;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _columns = new List<Column>();
+            _mockContext = new Mock<IColumnContext>();
+            _mockContext.Setup(p => p.Columns).Returns(_columns);
+            _repository = new ColumnRepository(_mockContext.Object);
+        }
+
+        [Test]
+        public void TestGetAllShouldReturnColumns()
+        {
+            var column = new Column(1, "Column 1");
+            _columns.Add(column);
+            var result = _repository.GetAll();
+            Assert.That(result.Single(), Is.EqualTo(column));
+        }
+
+        [Test]
+        public void TestAddShouldAddColumn()
+        {
+            var column = new Column(1, "Column 1");
+            _repository.Add(column);
+            Assert.That(_columns.Single(), Is.EqualTo(column));
+        }
+    }
+}
