490ea660f8096371ab7bc5219dabe12d02edf2c6
Matthew Renze <matthew@renzeconsulting.com>
2014-01-05 16:54:29 -0600

    Added click-once installer

--- DataExplorer/Properties/AssemblyInfo.cs
+++ DataExplorer/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DataExplorer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Interactive data visualization tool for exporatory data analysis.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataExplorer")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Renze Consulting")]
+[assembly: AssemblyProduct("Data Explorer")]
+[assembly: AssemblyCopyright("Copyright ©  Matthew Renze 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1")]
