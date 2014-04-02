fa36b047774faa74d316d8ba94492a20afee3785
Matthew Renze <matthew@renzeconsulting.com>
2014-04-02 06:46:22 -0500

    Changed version number to v0.2.0

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -3,5 +3,5 @@
 // NOTE: This file is used to automatically set the version number
 // NOTE: of all projects in the solution.  If you change the version number
 // NOTE: here, it will be reflected in all assemblies when you build the solution
-[assembly: AssemblyVersion("0.1.0.*")]
-//[assembly: AssemblyFileVersion("0.1.*")]
+[assembly: AssemblyVersion("0.2.0.*")]
+//[assembly: AssemblyFileVersion("0.2.*")]
